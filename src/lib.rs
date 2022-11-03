c0d22f9b5df0d3706c52da7b8a2c3b3fcffa7065
Sollimann <kristoffer.solberg@cognite.com>
2022-11-03 02:09:10 +0100

    make message public

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ extern crate log;
 mod codec;
 mod endpoint;
 mod errors;
-mod message;
+pub mod message;
 
 pub use crate::endpoint::{serve, Ack, Client, Endpoint, Response, Service, ServiceWithClient};
 pub use rmpv::{Integer, Utf8String, Value};
